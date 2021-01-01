f61c1c6dc4323a91393f26b47bcecf1dc59a083e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-01-01 13:20:46 -0300

    Instalar y configurar bootstrap con webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// Importar automáticamente estos módulos cuando otro módulo (i.e. bootstrap)
+// los trate de usar.
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
