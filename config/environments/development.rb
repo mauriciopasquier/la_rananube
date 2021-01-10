3322fdd900c17d7b48d3ccc4a1a48ad80650a016
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-01-10 18:07:21 -0300

    Mandar mails en desarrollo a mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,11 +37,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
-  # Default host for Devise
+  # Default host para Devise.
   config.action_mailer.default_url_options = {
     host: 'localhost',
     port: 3000
   }
+
+  # Mailcatcher smtp server (ver mails en localhost:1080).
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
