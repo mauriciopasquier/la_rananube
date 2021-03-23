02dbf732d89ec63daa29e2675848a64e13f0ba00
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-03-23 02:01:31 -0300

    Deshardcodear el nombre de la app en los tests

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,20 +4,20 @@ class ApplicationHelperTest < ActionView::TestCase
   describe ApplicationHelper do
     describe '#titulo_de_app' do
       it 'devuelve el nombre de la app por default' do
-        _(titulo_de_app).must_equal 'La Rananube'
+        _(titulo_de_app).must_equal Setting.nombre_de_app
       end
 
       it 'incorpora el título de página, si existe' do
         stub :titulo_de_pagina, 'Cosa' do
-          _(titulo_de_app).must_equal 'Cosa | La Rananube'
+          _(titulo_de_app).must_equal "Cosa | #{Setting.nombre_de_app}"
         end
 
         stub :titulo_de_pagina, '' do
-          _(titulo_de_app).must_equal 'La Rananube'
+          _(titulo_de_app).must_equal Setting.nombre_de_app
         end
 
         stub :titulo_de_pagina, nil do
-          _(titulo_de_app).must_equal 'La Rananube'
+          _(titulo_de_app).must_equal Setting.nombre_de_app
         end
       end
     end
