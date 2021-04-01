e14342ecbef48fb712c5061493f47782ab34c961
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-04-01 03:20:46 -0300

    Mostrar las notificaciones del sistema

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -21,5 +21,25 @@ class ApplicationHelperTest < ActionView::TestCase
         end
       end
     end
+
+    describe '#notificaciones' do
+      it 'prepara notificaciones para los tipos permitidos' do
+        mensajes = {
+          'success' => 'Éxito',
+          'error' => 'Fracaso',
+          'alert' => 'Alarma',
+          'notice' => 'Mensaje',
+          'inexistente' => 'Inexistente'
+        }
+
+        stub :flash, mensajes do
+          _(notificaciones).must_match 'Éxito'
+          _(notificaciones).must_match 'Fracaso'
+          _(notificaciones).must_match 'Alarma'
+          _(notificaciones).must_match 'Mensaje'
+          _(notificaciones).wont_match 'Inexistente'
+        end
+      end
+    end
   end
 end
