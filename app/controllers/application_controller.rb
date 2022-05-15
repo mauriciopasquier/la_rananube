3db36bc81db742eb7df55cb7a5682b5113756e41
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2022-05-15 04:10:53 -0300

    Security update (#12)
    * Actualizar node a lts/fermium y paquetes de js
    * Update gems minor version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # Superclase para todos los controladores.
 class ApplicationController < ActionController::Base
   # Sistema de permisos a través de clases de reglas ("policies").
-  include Pundit
+  include Pundit::Authorization
   # Paginación.
   include Pagy::Backend
 
